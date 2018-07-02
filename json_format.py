cca349e46daf754d2a793f9ff04a1033e28fb8f6
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-02 18:44:12 +0300

    Update json_format.py

--- json_format.py
+++ json_format.py
@@ -1,3 +1,16 @@
+# File: json_format.py
+# Description: Examples on how to process json files in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Examples on how to process json files in Python with help of json library // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Processing_json_files_in_Python (date of access: XX.XX.XXXX)
+
+
+
 # Working with files in json formats
 
 import json
@@ -42,45 +55,3 @@ with open("students.json", "r") as f:
 
 # Calculating the sum of all scores for the second student
 print(sum(data_again[1]["scores"]))
-
-
-# Implementing the task
-# As an input there is a description of classes in json format
-# Calculate the number of inheritance for each class
-# Results has to be represented in alphabetical order
-# Converting input string into json format and then into list of dictionaries
-# data_json = input()
-# data = json.loads(dat)
-
-# Tested data with a list of dictionaries in json format
-data1 = [{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
-data = [{"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}, {"name": "A", "parents": []}, {"name": "D", "parents":["C", "F"]}, {"name": "E", "parents":["D"]}, {"name": "F", "parents":[]}]
-
-# Creating a dictionary for storing the resulted information
-d = {}
-
-# Creating a list for sorting the future results in alphabetic order
-list_of_names = []
-
-# Going through all names and writing the all unique names of classes inside new dictionary
-for i in range(len(data)):
-    for x, y in data[i].items():
-        if x == "name":
-            if y not in d:
-                d[y] = 1
-                list_of_names += [y]
-
-# Going through all parents and adding number of met names of classes into appropriate key
-for i in range(len(data)):
-    for x, y in data[i].items():
-        if x == "parents":
-            for j in range(len(y)):
-                if y[j] in d:
-                    d[y[j]] += 1
-
-# Sorting list of names
-sorted(list_of_names)
-
-# Showing the result in alphabetic order
-for x in list_of_names:
-    print(x, ':', d[x])
